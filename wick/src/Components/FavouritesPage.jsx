03203adc3029bfca44e0642ac78cf4961585e025
Anthony <wangonguanthony@gmail.com>
2025-04-30 12:48:31 +0300

    Added logic to check whether there are favorites

--- wick/src/Components/FavouritesPage.jsx
+++ wick/src/Components/FavouritesPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-function FavouritesPage() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+function FavouritesPage({recipes}) {
+    const favoriteRecipes = recipes
+  
+    return (
+        <>
+        <div className="recipe-container">        
+          {favoriteRecipes.length > 0 ? (
+            favoriteRecipes.map(recipe => (
+              <RecipeCard 
+              key={recipe.id} 
+              recipe={recipe}
+               />
+            ))
+          ) : (
+            <p>No favorites yet. Add some from the homepage!</p>
+          )}
+        </div>
+        </>
+      );
+ }
 
 export default FavouritesPage
