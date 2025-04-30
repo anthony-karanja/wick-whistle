54f935ea33dd9995042758c6ef7b9717ece7fd4f
Anthony <wangonguanthony@gmail.com>
2025-04-30 12:55:50 +0300

    Added a restriction where you have to sign in to view the recipes

--- wick/src/Components/FavouritesPage.jsx
+++ wick/src/Components/FavouritesPage.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React from "react";
+import RecipeCard from "./RecipeCard";
 
-function FavouritesPage({recipes}) {
-    const favoriteRecipes = recipes
-  
+function FavouritesPage({ recipes, onFavoriteToggle, isSignedUp }) {
+  const favoriteRecipes = recipes.filter((recipe) => recipe.isFavorite);
+
+  if (!isSignedUp) {
     return (
-        <>
+      <div>
         <h1>Recipe Favorites</h1>
-        <div className="recipe-container">        
-          {favoriteRecipes.length > 0 ? (
-            favoriteRecipes.map(recipe => (
-              <RecipeCard 
-              key={recipe.id} 
-              recipe={recipe}
-               />
-            ))
-          ) : (
-            <p>No favorites yet. Add some from the homepage!</p>
-          )}
-        </div>
-        </>
-      );
- }
+        <p>You need to sign up to view your favorite recipes!</p>
+      </div>
+    );
+  }
 
-export default FavouritesPage
+  return (
+    <>
+      <h1>Recipe Favorites</h1>
+      <div className="recipe-container">
+        {favoriteRecipes.length > 0 ? (
+          favoriteRecipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              recipe={recipe}
+              isSignedUp={isSignedUp}
+              onFavoriteToggle={onFavoriteToggle}
+            />
+          ))
+        ) : (
+          <p>No favorites yet. Add some from the homepage!</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default FavouritesPage;
